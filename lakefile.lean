d1c4779a07c8775799a19a92ddd9bb4253a8a79c
Seasawher <tellagoodstory@icloud.com>
2024-03-03 21:54:49 +0900

    add more configurations

--- lakefile.lean
+++ lakefile.lean
@@ -5,12 +5,14 @@ package «bct» where
   -- Settings applied to both builds and interactive editing
   leanOptions := #[
     ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
-    ⟨`pp.proofs.withType, false⟩
+    ⟨`pp.proofs.withType, false⟩,
+    ⟨`autoImplicit, false⟩,
+    ⟨`relaxedAutoImplicit, false⟩
   ]
   -- add any additional package configuration options here
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git" @ "bf9724aa3f507588e226671283bd7561812ee67d"
 
 @[default_target]
 lean_lib «Bct» where
